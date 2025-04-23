7e048f650c81f9ebae60197d96dd343ba98e59b0
pedroLp70 <petviajante1@gmail.com>
2025-04-23 09:44:31 -0400

    Corrigido erro no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/copia-auxiliar-transporte-para-organizar/', // Nome exato do seu repositório no GitHub
+  base: '/copia-auxiliar-transporte-para-organizar/',  // Nome exato do seu repositório no GitHub
 });
